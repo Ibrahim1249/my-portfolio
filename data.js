1fb4982f3a4caa7440fce78c367fe273e30cfd6f
Ibrahim1249 <mominibrahim98.im85@gmail.com>
2024-09-04 13:27:24 +0530

    added project

---
+++ data.js
@@ -0,0 +1,223 @@
+import youtube from "./src/assets/images/Youtube.jpeg"
+import food from "./src/assets/images/Food.jpeg"
+import movie from "./src/assets/images/Movie.jpeg"
+import tax from "./src/assets/images/Tax.jpeg"
+import warehouse from "./src/assets/images/Warehouse.jpeg"
+import weather from "./src/assets/images/Weather.jpeg"
+import dashboard from "./src/assets/images/Dashboard.jpeg"
+import todo from "./src/assets/images/Todo.jpeg"
+import textUtils from "./src/assets/images/Text Utilts.jpeg"
+import cart from "./src/assets/images/Cart.jpeg"
+import gym from "./src/assets/images/Gym.jpeg"
+import notepad from "./src/assets/images/Notepad.jpeg"
+import geekFood from "./src/assets/images/GeekFood.jpeg"
+import quiz from "./src/assets/images/Quiz.jpeg"
+import password from "./src/assets/images/password.jpeg"
+import bank from "./src/assets/images/Bank.jpeg"
+import multiSearch from "./src/assets/images/Multisearch.jpeg"
+import phone from "./src/assets/images/phone.jpeg"
+import crypto from "./src/assets/images/Crypto.jpeg"
+import pokemon from "./src/assets/images/Pokemon.jpeg"
+import unPlash from "./src/assets/images/Unplash.jpeg"
+import aria from "./src/assets/images/Aria.jpeg"
+import camping from "./src/assets/images/Camping.jpeg"
+import crossword from "./src/assets/images/CrossWord.jpeg"
+import gymTrainer from "./src/assets/images/GymTraner.jpeg"
+
+
+
+export const projects = [
+  
+    {
+      category: "MERN Stack",
+      url: "#",
+      imgSrc: youtube,
+      imgAlt: "youtube",
+      title: "YouTube Project(In Progress)",
+    },
+    {
+      category: "MERN Stack",
+      url: "",
+      imgSrc: food,
+      imgAlt: "food delivery Project",
+      title: "Food Delivery Project",
+    },
+    {
+        category: "ReactJs",
+        url: "",
+        imgSrc: movie,
+        imgAlt: "movie Project",
+        title: "Movie Project",
+      },
+
+      {
+        category: "ReactJs",
+        url: "",
+        imgSrc: warehouse,
+        imgAlt: "warehouse Project",
+        title: "Warehouse Project",
+      },
+
+      {
+        category: "ReactJs",
+        url: "",
+        imgSrc: tax,
+        imgAlt: "tax Project",
+        title: "Tax Calculator Project",
+      },
+      {
+        category: "ReactJs",
+        url: "",
+        imgSrc: weather,
+        imgAlt: "Weather Project",
+        title: "Weather Project",
+      },
+      {
+        category: "ReactJs",
+        url: "",
+        imgSrc: dashboard,
+        imgAlt: "Dashboard Project",
+        title: "Dashboard Project",
+      },
+      {
+        category: "ReactJs",
+        url: "",
+        imgSrc: todo,
+        imgAlt: "todo Project",
+        title: "Todo List Project",
+      },
+      {
+        category: "ReactJs",
+        url: "",
+        imgSrc: textUtils,
+        imgAlt: "textUtils Project",
+        title: "TextUtils Project",
+      },
+      {
+        category: "ReactJs",
+        url: "",
+        imgSrc: cart,
+        imgAlt: "cart Project",
+        title: "Product Cart Project",
+      },
+      {
+        category: "ReactJs",
+        url: "",
+        imgSrc: gym,
+        imgAlt: "gym Project",
+        title: "Gym Project",
+      },
+      {
+        category: "ReactJs",
+        url: "",
+        imgSrc: notepad,
+        imgAlt: "notepad Project",
+        title: "Notepad Project",
+      },
+      {
+        category: "ReactJs",
+        url: "",
+        imgSrc: geekFood,
+        imgAlt: "geekFood Project",
+        title: "GeekFood Project",
+      },
+      {
+        category: "ReactJs",
+        url: "",
+        imgSrc: quiz,
+        imgAlt: "quiz Project",
+        title: "Quiz Project",
+      },
+      {
+        category: "ReactJs",
+        url: "",
+        imgSrc: password,
+        imgAlt: "password Project",
+        title: "Password Generator Project",
+      },
+      {
+        category: "ReactJs",
+        url: "",
+        imgSrc: bank,
+        imgAlt: "bank Project",
+        title: "Bank Project",
+      },
+      {
+        category: "JavaScript",
+        url: "",
+        imgSrc: multiSearch,
+        imgAlt: "multiSearch",
+        title: "Multi-Search Website",
+      },
+      {
+        category: "JavaScript",
+        url: "",
+        imgSrc: pokemon,
+        imgAlt: "pokemon",
+        title: "Pokemon Website",
+      },
+      {
+        category: "JavaScript",
+        url: "",
+        imgSrc: phone,
+        imgAlt: "phone",
+        title: "Mobile Phone Website",
+      },
+      {
+        category: "JavaScript",
+        url: "",
+        imgSrc: crypto,
+        imgAlt: "crypto",
+        title: "Crypto Website",
+      },
+      {
+        category: "JavaScript",
+        url: "",
+        imgSrc: unPlash,
+        imgAlt: "unPlash",
+        title: "Photo Gallery Website",
+      },
+      {
+        category: "HTML-CSS",
+        url: "",
+        imgSrc: crossword,
+        imgAlt: "crossword",
+        title: "Cross-Word Website Clone",
+      },
+      {
+        category: "HTML-CSS",
+        url: "",
+        imgSrc: aria,
+        imgAlt: "aria",
+        title: "Aria Website ",
+      },
+      {
+        category: "HTML-CSS",
+        url: "",
+        imgSrc: gymTrainer,
+        imgAlt: "gymTrainer",
+        title: "Gym Trainer Website",
+      },
+      {
+        category: "HTML-CSS",
+        url: "",
+        imgSrc: camping,
+        imgAlt: "camping",
+        title: "Camping Website",
+      },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ];
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,82 @@
-import React from 'react'
+import {projects} from "../../data.js"
+import { useState } from "react";
 
 function Project() {
+  const categories = ["All", "ReactJs", "JavaScript", "HTML-CSS"];
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [isSelectOpen, setIsSelectOpen] = useState(false);
+
+  const handleFilterClick = (category) => {
+    setSelectedCategory(category);
+    setIsSelectOpen(false); 
+  };
+
+  const handleSelectClick = () => {
+    setIsSelectOpen(!isSelectOpen); 
+  };
+  const filteredProjects = selectedCategory === "All" 
+    ? projects 
+    : projects.filter(project => project.category === selectedCategory);
+
   return (
-    <div>Project</div>
+    <>
+         <article className="portfolio" data-page="portfolio">
+      <header>
+        <h2 className="h2 article-title">Portfolio</h2>
+      </header>
+      <section className="projects">
+        <ul className="filter-list">
+          {categories.map((category) => (
+            <li className="filter-item" key={category}>
+              <button 
+                className={category === selectedCategory ? "active" : ""} 
+                onClick={() => handleFilterClick(category)}
+              >
+                {category}
+              </button>
+            </li>
+          ))}
+        </ul>
+        <div className="filter-select-box">
+          <button className="filter-select" data-select="" onClick={handleSelectClick}>
+            <div className="select-value" data-select-value="">
+              {selectedCategory}
+            </div>
+            <div className="select-icon">
+              <ion-icon name="chevron-down" />
+            </div>
+          </button>
+          {isSelectOpen && (
+            <ul className="select-list">
+              {categories.map((category) => (
+                <li className="select-item" key={category}>
+                  <button data-select-item="" onClick={() => handleFilterClick(category)}>
+                    {category}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <ul className="project-list">
+          {filteredProjects.map(({ category, url, imgSrc, imgAlt, title }) => (
+            <li className="project-item active" data-filter-item="" data-category={category} key={title}>
+              <a target="_blank" rel="noopener noreferrer" href={url}>
+                <figure className="project-img">
+                  <div className="project-item-icon-box">
+                    <ion-icon name="eye-outline" />
+                  </div>
+                  <img src={imgSrc} alt={imgAlt} loading="lazy" />
+                </figure>
+                <h3 className="project-title">{title}</h3>
+                <p className="project-category">{category}</p>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </article>
+    </>
   )
 }
 
